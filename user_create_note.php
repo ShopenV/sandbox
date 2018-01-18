b242d5a432fefa6a9cd91c6d8cfe21916d1027b7
ShopenV <33993102+ShopenV@users.noreply.github.com>
2018-01-18 11:41:53 +0300

    A form to add new notes to the blog

---
+++ user_create_note.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="it-blog.css">
+  <title>Create a note</title>
+</head>
+<body>
+   <form method="post" action="create_note.php">
+	    <input class="headline" type="text" name="headline" placeholder="Заголовок">	
+	    <textarea name="note" placeholder="Введите текст"></textarea>
+	    <br>
+	    <input class="submit" type="submit">
+   </form>
+</body>
+</html>
